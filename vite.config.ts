3cc284f5082bb36b86b80d70bc1cfa157870b397
noorsiddiqui-prog <noorsiddiqui048@gmail.com>
2024-07-16 18:24:33 +0500

    feat: chat app

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: process.env.BACKEND_PROXY_URL || "http://localhost:5000",
         changeOrigin: true
       }
     }
